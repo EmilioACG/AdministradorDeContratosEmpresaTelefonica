67712e26a4a8101e9396fd3957d323b002e54f44
EmilioCordovaG <emilioandres51@gmail.com>
2024-08-20 16:58:32 -0400

    Primer commit

---
+++ src/main/java/com/mycompany/administradordecontratosempresatelefonica/AdministradorDeContratosEmpresaTelefonica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.administradordecontratosempresatelefonica;
+
+/**
+ *
+ * @author Emilio
+ */
+public class AdministradorDeContratosEmpresaTelefonica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }|
+}

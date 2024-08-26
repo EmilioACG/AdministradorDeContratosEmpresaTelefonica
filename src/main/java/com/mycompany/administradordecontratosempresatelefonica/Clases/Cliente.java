5d411592b62df10545cef8fde6b38456832d3f07
EmilioCordovaG <emilioandres51@gmail.com>
2024-08-25 22:16:43 -0400

    Modificacion clases

---
+++ src/main/java/com/mycompany/administradordecontratosempresatelefonica/Clases/Cliente.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.administradordecontratosempresatelefonica.Clases;
+import java.util.Scanner;
+/**
+ *
+ * @author Emilio
+ */
+public class Cliente {
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String rut;
+    
+    
+   //Constructores
+   public Cliente(){
+       
+   }
+}
+
+

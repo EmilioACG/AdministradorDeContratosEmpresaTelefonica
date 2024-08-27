6a6f56866313a5af70509dd351ba2caa0dccd07d
MelissaRojas010 <melissa.rojas.g@mail.pucv.cl>
2024-08-27 18:22:03 -0400

    metodos print Clase Menu

--- src/main/java/com/mycompany/administradordecontratosempresatelefonica/AdministradorDeContratosEmpresaTelefonica.java
+++ src/main/java/com/mycompany/administradordecontratosempresatelefonica/AdministradorDeContratosEmpresaTelefonica.java
@@ -18,75 +18,9 @@ public class AdministradorDeContratosEmpresaTelefonica {
         int opcion;
         Scanner lector = new Scanner(System.in);
         
-        do{
-            printMenuGeneral();
-            opcion = lector.nextInt();
-            
-            switch (opcion) {
-                case 0:
-                    printSalida();
-                    break;
-                case 1:
-                    printMenuClientes();
-                    break;
-                case 2:
-                    printMenuContratos();
-                    break;
-                case 3:
-                    printMenuPlanes();
-                    break;
-                default:
-                    printError();
-                    break;
-            }
-                    
-            
-        }while(opcion != 0);
-        
+        Menu.menuGeneral();
         
         lector.close();
     }
     
-    public static void printMenuGeneral(){
-        System.out.println("------------------------------------");
-        System.out.println("1. Menú clientes");
-        System.out.println("2.- Menú contratos");
-        System.out.println("3.- Menú planes");
-        System.out.println("------------------------------------");;
-    }
-    
-    public static void printMenuClientes(){
-        System.out.println("------------------------------------");
-        System.out.println("Opciones clientes:");
-        System.out.println("  1.- Agregar cliente");
-        System.out.println("  2.- Mostrar cliente");
-        System.out.println("  3.- Modificar datos cliente");
-        System.out.println("  4.- Eliminar cliente");
-        System.out.println("  5.- Listar clientes");
-        System.out.println("  6.- Buscar cliente");
-        System.out.println("------------------------------------");
-    }
-    
-    public static void printMenuContratos(){
-        System.out.println("------------------------------------");
-        System.out.println("Opciones contratos:");
-        System.out.println("  1.- Agregar cliente");
-        System.out.println("  2.- Mostrar cliente");
-        System.out.println("  3.- Modificar datos cliente");
-        System.out.println("  4.- Eliminar cliente");
-        System.out.println("  6.- Buscar cliente");
-        System.out.println("------------------------------------");
-    }
-    
-    public static void printMenuPlanes(){
-        
-    }
-    
-    public static void printSalida(){
-        
-    }
-    
-    public static void printError(){
-    
-    }
 }

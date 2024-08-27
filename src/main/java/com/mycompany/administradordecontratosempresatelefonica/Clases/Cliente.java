b1ae63ff6aec31daee77c23d1025cf31ed7beefd
EmilioCordovaG <emilioandres51@gmail.com>
2024-08-27 17:51:00 -0400

    Se agrega set y get a clase clientes

--- src/main/java/com/mycompany/administradordecontratosempresatelefonica/Clases/Cliente.java
+++ src/main/java/com/mycompany/administradordecontratosempresatelefonica/Clases/Cliente.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package com.mycompany.administradordecontratosempresatelefonica.Clases;
 import java.util.Scanner;
 /**
@@ -13,6 +14,7 @@ public class Cliente {
     private String apellidoPaterno;
     private String apellidoMaterno;
     private int rut;
+    private boolean tieneContrato = false;
     
     
    //Constructores
@@ -25,17 +27,57 @@ public class Cliente {
    
    //Metodos
    
-   public void setDatosClientes(String nom,String apellP,String apellM,int num){
-       this.nombre = nom;
-       this.apellidoPaterno =  apellP;
-       this.apellidoMaterno = apellM;
-       this.rut = num;
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public boolean getTieneContrato() {
+        return tieneContrato;
+    }
+    
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public void setTieneContrato(boolean tieneContrato) {
+        this.tieneContrato = tieneContrato;
+    }
+    
+   
+   
+   public void modificarDatosClientes(String nom,String apellP,String apellM,int num,boolean tieneContr){
+       setNombre(nom);
+       setApellidoPaterno(apellP);
+       setApellidoMaterno(apellM);
+       setRut(num);
+       setTieneContrato(tieneContr);
    }
    
-   public void modificarDatos(String nom,String apellP,String apellM,int num)
-   {
-       setDatosClientes(nom,apellP,apellM,num);
-   }
 }
 
 
--- src/main/java/com/mycompany/administradordecontratosempresatelefonica/Clases/Menu.java
+++ src/main/java/com/mycompany/administradordecontratosempresatelefonica/Clases/Menu.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.administradordecontratosempresatelefonica.Clases;
-
 import java.util.Scanner;
 
 /**
